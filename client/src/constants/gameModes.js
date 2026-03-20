f8820658ce6e95d6c3ff1a23bce2c6cc4106c1aa
corey <coreybuchanan79@gmail.com>
2026-03-20 08:31:11 -0500

    feat(game-modes): align mode list with confirmed Warzone playlists

--- client/src/constants/gameModes.js
+++ client/src/constants/gameModes.js
@@ -1,13 +1,9 @@
 export const WARZONE_GAME_MODES = [
+    'Black Ops Royale',
+    'Battle Royale',
     'Battle Royale - Casual',
     'Battle Royale - Regular',
-    'Battle Royale - Ranked',
-    'Battle Royale', // legacy compatibility
     'Resurgence',
-    'Plunder',
-    'Lockdown',
-    'Bootcamp',
-    'Clash',
-    'Buy Back Royale',
-    'Loaded Resurgence'
+    'Ranked Play / Ranked Series',
+    'Extreme Resurgence Weekend (LTM)'
 ];
